ea8bdf463be5475d9c004a26f0c152607f021bfe
josin22 <233679057@qq.com>
2016-09-08 14:53:32 +0800

    init

---
+++ JSCatSwitchDemo/JSCatSwitchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSCatSwitchDemo
+//
+//  Created by 乔同新 on 16/9/8.
+//  Copyright © 2016年 乔同新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSCatSwitchDemo/JSCatSwitchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSCatSwitchDemo
+//
+//  Created by 乔同新 on 16/9/8.
+//  Copyright © 2016年 乔同新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
